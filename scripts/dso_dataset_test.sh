b02132e150c3d355967153da782a72a82ad58040
minebastiii <bastian261198@googlemail.com>
2023-12-08 16:23:01 +0100

    working dso; nproc core number for make

---
+++ scripts/dso_dataset_test.sh
@@ -0,0 +1,7 @@
+/workspace/dso/build/bin/dso_dataset \
+	files=../testdata/sequence_04/images.zip \
+	calib=../testdata/sequence_04/camera.txt \
+	gamma=../testdata/sequence_04/pcalib.txt \
+	vignette=../testdata/sequence_04/vignette.png \
+	preset=0 \
+	mode=0
