953217ecdfa57df15bc69d6dcc1a17da88fea38d
minebastiii <bastian261198@googlemail.com>
2024-02-13 22:07:41 +0100

    added orb_slam

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -4,10 +4,12 @@ chmod +x install_ros_base.sh
 chmod +x install_svo.sh
 chmod +x install_ptam.sh
 chmod +x install_dso.sh
+chmod +x install_orb_slam3.sh
 
 chmod +x start_svo.sh
 chmod +x start_ptam.sh
 chmod +x start_dso.sh
+chmod +x start_orb_slam3.sh
 
 chmod +x dso_dataset_test.sh
 
@@ -15,3 +17,4 @@ chmod +x dso_dataset_test.sh
 ./install_svo.sh
 ./install_ptam.sh
 ./install_dso.sh
+./install_orb_slam3.sh
---
+++ scripts/install_orb_slam3.sh
@@ -0,0 +1,2 @@
+#apptainer build ros_base.sif ros_base.def
+apptainer build --sandbox ../orb_slam3/ ../orb_slam3_ros.def
