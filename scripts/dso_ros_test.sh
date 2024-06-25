7f97e976f993714258bffeeccc9e4e2776dcdecd
minebastiii <bastian261198@googlemail.com>
2024-06-25 18:21:13 +0200

    added simple 2d array plot;

--- scripts/dso_ros_test.sh
+++ scripts/dso_ros_test.sh
@@ -1,7 +1,5 @@
 rosrun dso_ros dso_live image:=/usb_cam/image_raw \
     calib=../custom_config/dso_dji_cam_calib.yaml \
     savefile=../testdata/results/dso_traj.txt \
-    vignette=../testdata/visual_odometry_testdata/sequence_50/vignette.png
-
-    #calib=../testdata/visual_odometry_testdata/sequence_50/camera.txt \
-    #gamma=../testdata/visual_odometry_testdata/sequence_50/pcalib.txt \
+    vignette=../testdata/visual_odometry_testdata/sequence_50/vignette.png \
+    gamma=../testdata/visual_odometry_testdata/sequence_50/pcalib.txt
