cf18af0c7c56dbdc13b0462fe749ac3142831475
minebastiii <bastian261198@googlemail.com>
2024-06-25 17:36:01 +0200

    updated all scripts and added some calib files;

--- scripts/dso_ros_test.sh
+++ scripts/dso_ros_test.sh
@@ -1,4 +1,7 @@
 rosrun dso_ros dso_live image:=/usb_cam/image_raw \
-    calib=../testdata/visual_odometry_testdata/sequence_50/camera.txt \
-    gamma=../testdata/visual_odometry_testdata/sequence_50/pcalib.txt \
+    calib=../custom_config/dso_dji_cam_calib.yaml \
+    savefile=../testdata/results/dso_traj.txt \
     vignette=../testdata/visual_odometry_testdata/sequence_50/vignette.png
+
+    #calib=../testdata/visual_odometry_testdata/sequence_50/camera.txt \
+    #gamma=../testdata/visual_odometry_testdata/sequence_50/pcalib.txt \
