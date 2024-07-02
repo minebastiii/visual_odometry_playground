481538995d1bb4ddb072c25453d49d93965450aa
minebastiii <bastian261198@googlemail.com>
2024-07-02 18:02:15 +0200

    some sync tests; rudimentary visual flow;

--- scripts/dso_ros_test.sh
+++ scripts/dso_ros_test.sh
@@ -1,5 +1,6 @@
 rosrun dso_ros dso_live image:=/usb_cam/image_raw \
     calib=../custom_config/dso_dji_cam_calib.yaml \
     savefile=dso_traj.txt \
-    vignette=../testdata/visual_odometry_testdata/sequence_50/vignette.png \
     gamma=../custom_config/dji_cam_pcalib.txt
+
+    #vignette=../testdata/visual_odometry_testdata/sequence_50/vignette.png \
